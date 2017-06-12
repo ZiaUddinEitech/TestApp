3edc24472d47e0bc5f016ce01f62dc72598751eb
Tahmid-PC\Tahmid <zia@enhancedgroupbd.com>
2017-06-12 12:24:47 +0600

    This is for test purpose

---
+++ category.php
@@ -0,0 +1,15 @@
+<?php
+require '../wp-load.php';
+$cat=get_all_category_ids();
+ $args = array( 'category' => 1,'posts_per_page' => 3, 'offset'=>1, 'post_type' =>  'post' ); 
+  $postslist = get_posts( $args ); 
+	$i=0;
+	$post_data=array();
+	foreach ($postslist as $post) :  setup_postdata($post); 
+	$post_data['title'][$i]=get_the_title();
+	$post_data['date'][$i]=get_the_time('d-m-Y');
+	$i++;
+	endforeach;
+echo json_encode($post_data);
+
+?>
